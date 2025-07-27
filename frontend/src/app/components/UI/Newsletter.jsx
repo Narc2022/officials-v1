8ad9dd0207f4de89e428e6e66e379fb312447e5b
sachin <sachinnarofficials@gmail.com>
2025-07-27 21:53:03 +0530

    footer started

--- frontend/src/app/components/UI/Newsletter.jsx
+++ frontend/src/app/components/UI/Newsletter.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "../../styles/newsletter.css";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <section className="newsletter">
+      <div className="container">
+        <div className="newsletter__wrapper">
+          <div className="newsletter_content">
+            <h6 className="subtitle">Let's work</h6>
+            <h2 className="">
+              Explore the <span className="highlight">hidden</span> ideas and
+              subscribe!
+            </h2>
+          </div>
+          <div className="newsletter__form">
+            <input id="f" type="email" placeholder="Email" />
+            <button className="secondary__btn">Subscribe Now</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
